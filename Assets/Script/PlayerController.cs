00c126898fd18df2b2e2aecc132e02749f3c6ff4
pochite <gomatamago0206@gmail.com>
2018-08-28 22:01:55 +0900

    playerControllerを頭悪くしました

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -6,11 +6,21 @@ public class PlayerController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
+    private void kira()
+    {
+        float testA = 1f;
+        float testB = 2f;
+        testA = testB;
+        testB = testA;
+    }
 }
